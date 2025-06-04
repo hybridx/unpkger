0b5a059ff130d9e8e433b68681baad9a8e45a202
hybridx <hybridx18@gmail.com>
2025-06-05 02:18:08 +0530

    fix: update vite config to use dynamic base path from GitHub repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/unpkger/',
+  base: `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || 'unpkger'}/`,
   build: {
     outDir: 'dist'
   }
